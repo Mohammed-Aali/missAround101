0501166d62365ca67b121059f2bcb225d43bd9d4
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-22 08:32:12 +0300

    another solution to the 3rd challange. the issue described erlier is resolved; i++ return the value of i prior to addition and the value of ++i returns it after the addition has been completed.

--- Challenges/veryEasy.js
+++ Challenges/veryEasy.js
@@ -1,6 +1,6 @@
 // 3rd challenge on edabit 
 // Create a function that takes a number as an argument, increments the number by +1 and returns the result.
 function additon(num) {
-    return num += 1;
+    return ++num;
 }
 console.log(additon(2));
