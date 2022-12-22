dddaa85dbd6a503ed0c979b3b2688cd430a2f6a9
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-22 08:09:11 +0300

    The second challange completed. Oh, and btw there was an error in the linking to the easy.js file, due to me changing the name of the file, I fixed it.

--- Challenges/veryEasy.js
+++ Challenges/veryEasy.js
@@ -1,5 +1,7 @@
-// First challenge on edabit 
-// Write a fucntion that returns "hello edabit.com"
-function hello() {
-    return "hello edabit.com"
+// second challenge on edabit 
+// Write a function that takes the base and height of a triangle and return its area.
+function triArea(base, height) {
+    let area = (base * height) / 2;
+    return area;
 }
+console.log(triArea(2, 4))
