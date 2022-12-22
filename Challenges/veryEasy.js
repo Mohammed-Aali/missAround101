cf82e9fa79d5a119de713c3048aa7854a12bcbd4
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-22 11:51:30 +0300

    changing the level challenge to easy instead of very easy

--- Challenges/veryEasy.js
+++ Challenges/veryEasy.js
@@ -1,7 +1,8 @@
 //<========== challenge on edabit ==========>
 // Write a function that returns the string "something" joined with a space " " and the given argument a.
-function giveMeSomething(a) {
-    return `something ${a}` 
+function howManySeconds(hour) {
+    return hour * 60 * 60;
+     
 }
     
-console.log(giveMeSomething("is better than nothing"));
+console.log(howManySeconds(2));
