7a5aefb7c949df9204aea58523623e6a480dd31d
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-22 11:56:26 +0300

    already did this challenge

---
+++ Challenges/easy.js
@@ -0,0 +1,6 @@
+//<========== challenge on edabit ==========>
+//Write a function that takes the base and height of a triangle and return its area.
+function triArea(base, height) {
+    return (base * height) / 2;
+}
+console.log(triArea(6, 8))
