30da7c58ef4396d5bec19cc1cf8f04677a78b562
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-22 08:49:22 +0300

    this was supposed to be easy, lol.

--- Challenges/veryEasy.js
+++ Challenges/veryEasy.js
@@ -1,6 +1,9 @@
-// 3rd challenge on edabit 
-// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
-function additon(num) {
-    return ++num;
+// challenge on JSCodebox cuz edabit won't load
+// Given are two arrays a and b, both have three elements. Return a new array of length 2 containing both middle (index 1) elements of the arrays.
+let a = [1,2,4];
+let b = [1,2,4];
+function goldenMiddle(a, b) {
+  let c = [a[1], b[1]]
+  return c;
 }
-console.log(additon(2));
+console.log(goldenMiddle(a,b));
