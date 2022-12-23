23f9b852bc2b659b9e066eaa5c14dc2e7c0c7007
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-23 05:36:13 +0300

    There were many sulotions to this problems but i copied this one since it makes most sense: The proplem i encountered with this one is the good old 'return statement' placement.

--- Challenges/easy.js
+++ Challenges/easy.js
@@ -1,14 +1,10 @@
 //<========== challenge on edabit ==========>
-//A decimal number can be represented as a sequence of bits. To illustrate:
-function bitwiseAND(n1, n2) {
-	return (n1 & n2);
+//Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+function addUp(b) {
+	let sum = 0;
+	for (let i = 0; i <= b; i++) {
+		sum += i;
+	}
+	return sum;
 }
-
-function bitwiseOR(n1, n2) {
-	return (n1 | n2);
-}
-
-function bitwiseXOR(n1, n2) {
-	return (n1 ^ n2);
-}
-console.log(bitwiseOR(6, 8))
+console.log(addUp(4))
--- Challenges/veryEasy.js
+++ Challenges/veryEasy.js
@@ -1,8 +1,4 @@
 //<========== challenge on edabit ==========>
 // Write a function that returns the string "something" joined with a space " " and the given argument a.
-function howManySeconds(hours) {
-    return hours * 60 * 60;
-     
-}
-    
-console.log(howManySeconds(2));
+
+console.log();
