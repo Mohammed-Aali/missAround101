99ead897f0c09505b2cd6726fb9256f22a64ebb7
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-25 12:02:33 +0300

    the battle ship game inital v -1

--- headFirstJavaScript/battleship.js
+++ headFirstJavaScript/battleship.js
@@ -6,24 +6,17 @@ let hits = 0;
 let guesses = 0;
 let isSunk = false;
 
-// while ( isSunk == false ) {
-//    guess = prompt("Ready, aim, fire! (enter a number 0-6):");
-//    if (guess < 0 || guess > 6) {
-//     alert("Please enter a valid cell number!");
-//    } else {
-//     guesses = guesses + 1;
-//    } 
+while ( isSunk == false ) {
+   guess = prompt("Ready, aim, fire! (enter a number 0-6):");
+   if (guess < 0 || guess > 6) {
+    alert("Please enter a valid cell number!");
+   } else {
+    guesses = guesses + 1;
 
-//    if (guess == location1) {
-//     hits = hits + 1;
-//    } else if (guess == location2){
-//     hits = hits + 1;
-//    } else if (guess == location3){
-//     hits = hits + 1;
-//    }
-// }
-guess = 1;
-if (guess == location1 || guess == location2 || guess == location3) {
+    if (guess == location1 || guess == location2 || guess == location3) {
     hits = hits + 1;
+    }
+
+   }
+   
 }
-console.log(hits);
