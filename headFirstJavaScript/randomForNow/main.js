71565816b88720f3204b84f96be043d15097d15a
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-26 11:56:10 +0300

    the end of todays session.

--- headFirstJavaScript/randomForNow/main.js
+++ headFirstJavaScript/randomForNow/main.js
@@ -1,13 +1,2 @@
-function whatShallIWear(temp) {
-    if (temp < 60) {
-        console.log("wear a jacket");
-    } else if (temp < 70 ) {
-        console.log("wear a sweater");
-    } else {
-        console.log("wear a t-shirt");
-    }
-};
 
-whatShallIWear(50);
-whatShallIWear(80);
-whatShallIWear(60);
+
