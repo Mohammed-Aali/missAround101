0d96230f6e6b015d8eaba1546dafedd0a9645792
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-26 05:39:34 +0300

    complete logic behind battleship game

--- headFirstJavaScript/battleship.js
+++ headFirstJavaScript/battleship.js
@@ -10,13 +10,26 @@ while ( isSunk == false ) {
    guess = prompt("Ready, aim, fire! (enter a number 0-6):");
    if (guess < 0 || guess > 6) {
     alert("Please enter a valid cell number!");
-   } else {
+    } else {
     guesses = guesses + 1;
 
     if (guess == location1 || guess == location2 || guess == location3) {
     hits = hits + 1;
+    alert("Hit!") 
+
+    if(hits == 3){
+    isSunk = true;
+    alert("You sank my battle ship :'(")
+    } 
+7
+    
+    } else {
+    alert("Miss");
     }
 
    }
-   
+
 }
+
+let stats = "You took " + guesses + " guesses to sink the battleship, which means your shooting accuracy was " + (3/guesses) * 100 + "%";
+alert(stats);
