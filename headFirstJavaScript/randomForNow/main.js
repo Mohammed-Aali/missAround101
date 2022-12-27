3576034c4e40aacbf7b1218ab78611787754af1d
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-27 10:39:50 +0300

    first use of the for loop in the book

--- headFirstJavaScript/randomForNow/main.js
+++ headFirstJavaScript/randomForNow/main.js
@@ -1,9 +1,8 @@
 let products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
 let hasBubbleGum = [false, false, false, true];
-let i = 0;
-while ( i < products.length ) {
+
+for (let i = 0; i < products.length; i++) {
     if (hasBubbleGum[i]) {
-        console.log(`${products[i]} contains bubblegum`)
+        console.log(`${products[i]} contains bubblegum`);
     }
-    i = i + 1;
 }
