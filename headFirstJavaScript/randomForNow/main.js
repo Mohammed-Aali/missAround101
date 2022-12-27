a86ec78f2cf7bc9259135c13b2ab254bbb72b721
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-27 10:21:20 +0300

    bubble gum examples test code

--- headFirstJavaScript/randomForNow/main.js
+++ headFirstJavaScript/randomForNow/main.js
@@ -1,9 +1,9 @@
-let scores = [60, 50, 60, 58, 54,   54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
-
-let output;
+let products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
+let hasBubbleGum = [false, false, false, true];
 let i = 0;
-while ( i < scores.length) {
-    output = "bubble solution #" + i + " score:" + scores[i]
-    console.log(output);
+while ( i < products.length ) {
+    if (hasBubbleGum[i]) {
+        console.log(`${products[i]} contains bubblegum`)
+    }
     i = i + 1;
 }
