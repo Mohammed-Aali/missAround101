bffbc6484255de6081a80a05b17715baf5182e37
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-27 08:25:37 +0300

    phrasomatic app for slick talkers

--- headFirstJavaScript/randomForNow/main.js
+++ headFirstJavaScript/randomForNow/main.js
@@ -1,14 +1,11 @@
-let balance = 10500;
-let cameraOn = true;
-
-function steal(balance, amount) {
-    cameraOn = false;
-    if (amount < balance) {
-        balance = balance - amount;
+function makePhrases() {
+    var words1 = ["24/7", "multi-tier", "30,000 foot", "B-to-B", "win-win"];
+    var words2 = ["empowered", "value-added", "oriented", "focused", "aligned"];
+    var words3 = ["process", "solution", "tipping-point", "strategy", "vision"];
+    var rand1 = Math.floor(Math.random() * words1.length);
+    var rand2 = Math.floor(Math.random() * words2.length);
+    var rand3 = Math.floor(Math.random() * words3.length);
+    var phrase = words1[rand1] + " " + words2[rand2] + " " + words3[rand3];
+    alert(phrase);
     }
-    return amount;
-    cameraOn = true;
-}
-
-let amount = steal(balance, 1250);
-alert("Criminal: you stole " + amount + "!");
+    makePhrases()
