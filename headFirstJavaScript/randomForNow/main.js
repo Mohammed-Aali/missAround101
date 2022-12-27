99d1ac1680d20b87d6ce0fd0052e5150a0cac589
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-27 06:18:22 +0300

    the clunk counter machine:

--- headFirstJavaScript/randomForNow/main.js
+++ headFirstJavaScript/randomForNow/main.js
@@ -1,2 +1,35 @@
+let clunckCounter = 0;
+
+function clunk(times) {
+    let num = times;
+    while (num > 0) {
+        display("clunk");
+        num = num - 1;
+    }
+}
 
 
+function thingAmaJug(size) {
+    let facky = 1;
+    clunckCounter = 0;
+    if (size == 0) {
+        display("clank");
+    } else if ( size == 1 ) {
+        display("thunk");
+    } else {
+        while (size > 1) {
+            facky = facky + 1;
+            size = size - 1;
+        }
+        clunk(facky);
+    }
+}
+
+function display(output) {
+    console.log(output);
+    clunckCounter = clunckCounter + 1;
+}
+
+
+thingAmaJug(4);
+console.log(clunckCounter);
