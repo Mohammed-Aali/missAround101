3d2771b630f359853010e51e63a2639f99cc159f
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-27 11:56:09 +0300

    bubbles after a little work

--- headFirstJavaScript/randomForNow/main.js
+++ headFirstJavaScript/randomForNow/main.js
@@ -1,10 +1,19 @@
-var scores = [60, 50, 60, 58, 54, 54,
+let scores = [60, 50, 60, 58, 54, 54,
             58, 50, 52, 54, 48, 69,
             34, 55, 51, 52, 44, 51,
             69, 64, 66, 55, 52, 61,
             46, 31, 57, 52, 44, 18,
             41, 53, 55, 61, 51, 44];
+let highScore = 0;
+let output;
 for (let i = 0; i < scores.length; i++) {
-    let output = `bubble sulotion#${i} Score:${scores[i]}`
+    output = `bubble sulotion#${i} Score:${scores[i]}`
     console.log(output);
+
+    if (scores[i] > highScore) {
+        highScore = scores[i];
+    }
+    
 }
+console.log(`Bubbles tests: ${scores.length}`);
+console.log(`Highest bubble score: ${highScore}`);
