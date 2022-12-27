1a04ce3d9d891b2af91d80584f0c1edcde47faef
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-27 07:54:54 +0300

    about the machine i did erlier there was an error somewhere but i dont know where exactly, anyway, this is the steal function

--- headFirstJavaScript/randomForNow/main.js
+++ headFirstJavaScript/randomForNow/main.js
@@ -1,35 +1,14 @@
-let clunckCounter = 0;
+let balance = 10500;
+let cameraOn = true;
 
-function clunk(times) {
-    let num = times;
-    while (num > 0) {
-        display("clunk");
-        num = num - 1;
+function steal(balance, amount) {
+    cameraOn = false;
+    if (amount < balance) {
+        balance = balance - amount;
     }
+    return amount;
+    cameraOn = true;
 }
 
-
-function thingAmaJug(size) {
-    let facky = 1;
-    clunckCounter = 0;
-    if (size == 0) {
-        display("clank");
-    } else if ( size == 1 ) {
-        display("thunk");
-    } else {
-        while (size > 1) {
-            facky = facky + 1;
-            size = size - 1;
-        }
-        clunk(facky);
-    }
-}
-
-function display(output) {
-    console.log(output);
-    clunckCounter = clunckCounter + 1;
-}
-
-
-thingAmaJug(4);
-console.log(clunckCounter);
+let amount = steal(balance, 1250);
+alert("Criminal: you stole " + amount + "!");
