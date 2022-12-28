520be4bec9f7d9399069e725970034226a9b2f68
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-28 06:54:13 +0300

    code REFACTORED into funcitons

--- headFirstJavaScript/randomForNow/main.js
+++ headFirstJavaScript/randomForNow/main.js
@@ -4,23 +4,35 @@ let scores = [60, 50, 60, 58, 54, 54,
             69, 64, 66, 55, 52, 61,
             46, 31, 57, 52, 44, 18,
             41, 53, 55, 61, 51, 44];
-let highScore = 0;
-let output;
-let bestSolutions = [];
-for (let i = 0; i < scores.length; i++) {
-    output = `bubble sulotion#${i} Score:${scores[i]}`
-    console.log(output);
 
-    if (scores[i] > highScore) {
-        highScore = scores[i];
+function printAndGetHighScore(array) {
+    let highScore = 0;
+    let output;
+    for (let i = 0; i < array.length; i++) {
+        output = `bubble solution#${i} score: ${scores[i]}`;
+        console.log(output);
+
+        if (array[i] > highScore) {
+            highScore = array[i];
+        }
     }
+    return highScore;
 }
+
+let highScore = printAndGetHighScore(scores);
 console.log("Bubbles tests: " + scores.length);
 console.log("Highest bubble score: " + highScore);
 
-for (let i = 0; i < scores.length; i++) {
-    if (scores[i] == highScore) {
-        bestSolutions[bestSolutions.length] = i;
-    } 
+function getBestSolutions(array, highScore) {
+    let bestSolutions = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == highScore) {
+            bestSolutions[bestSolutions.length] = i;
+            // bestSolutions.push(i);
+        } 
+    }
+    return bestSolutions;
 }
+
+let bestSolutions = getBestSolutions(scores, highScore);
 console.log("Solutions with the highest score: " + bestSolutions);
