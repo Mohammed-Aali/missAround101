a20c42499c369720f6eb2265d725e584d2ed3134
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-28 09:02:47 +0300

    i had a bit of an issue with the last if statment and i figured it out, it took me a while sadly but i still figured it out eventually, my math needs some working it seems.

--- headFirstJavaScript/randomForNow/main.js
+++ headFirstJavaScript/randomForNow/main.js
@@ -5,6 +5,15 @@ let scores = [60, 50, 60, 58, 54, 54,
             46, 31, 57, 52, 44, 18,
             41, 53, 55, 61, 51, 44];
 
+let costs = [.25, .27, .25, .25, .25, .25, 
+            33, .31, .25, .29, .27, .22, 
+            31, .25, .25, .33, .21, .25, 
+            .25, .25, .28, .25, .24, .22,
+            .20, .25, .30, .25, .24, .25, 
+            .25, .25, .27, .25, .26, .29];
+console.log(costs[11]);
+console.log(costs[18]);
+
 function printAndGetHighScore(array) {
     let highScore = 0;
     let output;
@@ -19,10 +28,6 @@ function printAndGetHighScore(array) {
     return highScore;
 }
 
-let highScore = printAndGetHighScore(scores);
-console.log("Bubbles tests: " + scores.length);
-console.log("Highest bubble score: " + highScore);
-
 function getBestSolutions(array, highScore) {
     let bestSolutions = [];
     for (let i = 0; i < array.length; i++) {
@@ -34,5 +39,27 @@ function getBestSolutions(array, highScore) {
     return bestSolutions;
 }
 
+
+
+function getMostEffectiveSolution(array1, array2, highScore) {
+    let cost = 1;
+    let k;
+    for (let i = 0; i < array1.length; i++) {
+    if (array1[i] == highScore) {
+        if (cost > array2[i]) {
+            k = i;
+            cost = array2[k]
+        }
+    }
+    }
+    return k;
+}
+
+
+let highScore = printAndGetHighScore(scores);
+console.log("Bubbles tests: " + scores.length);
+console.log("Highest bubble score: " + highScore);
 let bestSolutions = getBestSolutions(scores, highScore);
 console.log("Solutions with the highest score: " + bestSolutions);
+let mostCostEffective = getMostEffectiveSolution(scores, costs, highScore);
+console.log(`bubble solution#${mostCostEffective} is the most cost effective`);
