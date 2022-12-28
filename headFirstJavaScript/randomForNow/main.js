e25072c7e050d194db42ea23e2219c9a2f2fa8a5
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-28 06:18:42 +0300

    the bubble thingie b4 we modified it so that it uses a function.

--- headFirstJavaScript/randomForNow/main.js
+++ headFirstJavaScript/randomForNow/main.js
@@ -6,6 +6,7 @@ let scores = [60, 50, 60, 58, 54, 54,
             41, 53, 55, 61, 51, 44];
 let highScore = 0;
 let output;
+let bestSolutions = [];
 for (let i = 0; i < scores.length; i++) {
     output = `bubble sulotion#${i} Score:${scores[i]}`
     console.log(output);
@@ -13,7 +14,13 @@ for (let i = 0; i < scores.length; i++) {
     if (scores[i] > highScore) {
         highScore = scores[i];
     }
-    
 }
-console.log(`Bubbles tests: ${scores.length}`);
-console.log(`Highest bubble score: ${highScore}`);
+console.log("Bubbles tests: " + scores.length);
+console.log("Highest bubble score: " + highScore);
+
+for (let i = 0; i < scores.length; i++) {
+    if (scores[i] == highScore) {
+        bestSolutions[bestSolutions.length] = i;
+    } 
+}
+console.log("Solutions with the highest score: " + bestSolutions);
