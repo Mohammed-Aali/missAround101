301dfb1b20b3c50da4b58d0d5d3b869fd7f7c5bc
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-29 06:49:41 +0300

    the auto-o-matic car making machine from the guy with the huge cigar.

--- headFirstJavaScript/randomForNow/main.js
+++ headFirstJavaScript/randomForNow/main.js
@@ -1,23 +1,32 @@
-let taxi = { 
-    make: "Webville Motors",
-    model: "Taxi",
-    year: 1955,
-    color: "yellow",
-    passengers: 4,
-    convertible: false,
-    mileage: 281341
-   };
-   function prequal(car) {
-    if (car.mileage > 10000) {
-    return false;
-    } else if (car.year > 1960) {
-    return false;
+
+function makeCar () {
+    let make = ["chevy", "toyota", "lexus", "honda", "Pagani", "ferrari"];
+    let models = ["corvette", "camery", "ls-500", "civic", "huaira", "laferrari"];
+    let years = [2013, 2022, 2012, 2023, 2018, 2010];
+    let colors = ["red", "blue", "tan", "yellow", "white"];
+    let convertible = [true, false];
+
+    let rand1 = Math.floor(Math.random() * make.length);
+    let rand2 = Math.floor(Math.random() * models.length);
+    let rand3 = Math.floor(Math.random() * years.length);
+    let rand4 = Math.floor(Math.random() * colors.length);
+    let rand5 = Math.floor(Math.random() * 2);
+    let rand6 = Math.floor(Math.random() * 5 ) + 1;
+
+    let car = {
+        make: make[rand1],
+        model: models[rand2],
+        year: years[rand3],
+        color: colors[rand4],
+        passengers: rand6,
+        convertible: convertible[rand5]
     }
-    return true;
-   }
-   let worthALook = prequal(taxi);
-   if (worthALook) {
-    console.log("You gotta check out this " + taxi.make + " " + taxi.model);
-   } else {
-    console.log("You should really pass on the " + taxi.make + " " + taxi.model);
-   }
+
+    return car;
+}
+
+function displayCar(car) {
+    console.log(`Your new car is a ${car.year} ${car.make} ${car.model}`);
+};
+
+displayCar(makeCar());
