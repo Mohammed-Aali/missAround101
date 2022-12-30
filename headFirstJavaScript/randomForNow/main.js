3db9a9fc220fd298d3a56fe3f56a7141157a8f99
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-30 08:47:27 +0300

    the planet and the document objecet model. Apetizers.

--- headFirstJavaScript/randomForNow/main.js
+++ headFirstJavaScript/randomForNow/main.js
@@ -1,22 +1,3 @@
-var eightBall = { index: 0, 
-    advice: ["yes", "no", "maybe", "not a chance"],
-    shake: function() { 
-    this.index = this.index + 1;
-    if (this.index >= this.advice.length) {
-    this.index = 0;
-    }
-    },
-    look: function() {
-    return this.advice[this.index];
-    }
-};
-
-eightBall.shake();
-console.log(eightBall.look())
-eightBall.shake();
-console.log(eightBall.look())
-eightBall.shake();
-console.log(eightBall.look())
-eightBall.shake();
-console.log(eightBall.look())
-
+let planet = document.getElementById("greenplanet");
+planet.innerHTML = "RED ALERT: hit by phaser fire";
+// you can turn back pages but not time
