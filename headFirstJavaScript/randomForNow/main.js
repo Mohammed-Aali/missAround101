bfa5f47f1a6ba7440e2a96131e5809a7b428365e
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-30 10:39:48 +0300

    songs with changed attribute by setting them new ones to class: red text.

--- headFirstJavaScript/randomForNow/main.js
+++ headFirstJavaScript/randomForNow/main.js
@@ -1,7 +1,19 @@
-function init() {
-    let planet = document.getElementById("greenplanet");
-    planet.innerHTML = "RED ALERT: hit by phaser fire";
-};
+function addSongs() {
+    let song1 = document.getElementById("song1");
+    let song2 = document.getElementById("song2");
+    let song3 = document.getElementById("song3");
 
-window.onload = init;
+    song1.innerHTML = "Blue Sued strings, by Elvis Pagely";
+    song1.setAttribute("class", "redtext");
+    song2.innerHTML = "Young God, by Russ";
+    let c = song1.getAttribute("ass");
+    song3.innerHTML = "Candy shop, By 50cents";
+    
+    if (c == null) {
+        console.log(`Hmm, strange there seems to be no attribute.`)
+    } else {
+        console.log(`found it ${c}`);
+    }
+}
 
+window.onload = addSongs;
