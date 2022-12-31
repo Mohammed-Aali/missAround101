ad492b9718e6f94d71c2b4a019511a52e8b5125f
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-31 08:04:13 +0300

    the parking lot example with earl the cowboy

--- headFirstJavaScript/randomForNow/main.js
+++ headFirstJavaScript/randomForNow/main.js
@@ -1,5 +1,37 @@
-if ( 99 == "99") {
-    console.log(`A number is equal to a string`);
-} else {
-    console.log(`No way a number is equal to a string`);
-}
+function findCarInLot(car) {
+    let i = 0;
+   while(i < lot.length){
+    if (car === lot[i]) {
+    return i;
+    }
+    i++;
+    }
+    return -1;
+   }
+   var chevy = {
+    make: "Chevy", 
+    model: "Bel Air"
+   };
+   var taxi = {
+    make: "Webville Motors", 
+    model: "Taxi"
+   };
+   var fiat1 = {
+    make: "Fiat", 
+    model: "500"
+   };
+   var fiat2 = {
+    make: "Fiat", 
+    model: "500"
+   };
+   var lot = [chevy, taxi, fiat1, fiat2];
+
+let loc1 = findCarInLot(fiat2);
+console.log(loc1);
+let loc2 = findCarInLot(taxi);
+console.log(loc2);
+let loc3 = findCarInLot(chevy);
+console.log(loc3);
+let loc4 = findCarInLot(fiat1);
+console.log(loc4);
+   
