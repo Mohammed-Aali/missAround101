eb1ec772e51b0fd3d85637f6dee43cfed8865975
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-31 11:56:51 +0300

    the mystic ways of the object string. it was kinda confusing not gonna lie

--- headFirstJavaScript/randomForNow/main.js
+++ headFirstJavaScript/randomForNow/main.js
@@ -1,17 +1,37 @@
-let mo = "mohammed ali al ali";
-let i = 0;
-while (i < mo.length) {
-    if (mo.charAt(i) === "a") {
-        console.log(`this mo has an a at ${i}`);
+function validate(phoneNumber) {
+    if (phoneNumber.length > 8 || phoneNumber < 7){
+        return false
     }
-    i++
+    for(let i = 0; i < phoneNumber.length; i++){
+        if(i === 3) {
+            if(phoneNumber.length === 8 && phoneNumber.charAt(i) !== "-" ) {
+                return false;
+            }
+        } else if (phoneNumber.length === 7 && isNaN(phoneNumber.charAt(i))) {
+            return false;
+        } else if (isNaN(phoneNumber.charAt(i))) {
+            return false;
+        }
+    }
+    return true;
 }
 
-let index = mo.indexOf("dog", 10);
-console.log(`there is an ali at ${index} just chilling.`);
-
-let val = mo.substring(9);
-console.log(`the substring is ${val}`);
-
-let vals = mo.split(" ");
-console.log(`Split array is`, vals);
+function validator(phoneNumber) {
+    if (phoneNumber.length > 8 || phoneNumber < 7) {
+        console.log(`the phone number isnt equal to 8 or 7`)
+        return false;
+    }
+    // let first = phoneNumber.substring(0,3);
+    // console.log(first);
+    // let second = phoneNumber.substring(phoneNumber.length - 4);
+    // console.log(second);
+    let arr = phoneNumber.split(phoneNumber.charAt(3));
+    if (isNaN(arr[0]) || isNaN(arr[1])) {
+        return false;
+    } else if (phoneNumber.length === 8) {
+        return (phoneNumber.charAt(3) === "-")
+    }
+    return true;
+}
+console.log(validate("000-4907"));
+console.log(validator("007-1234"));
