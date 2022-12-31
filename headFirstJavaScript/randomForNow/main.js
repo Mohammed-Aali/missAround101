32cc58911e92d83e2eeb2e331772937e8c4510be
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-31 08:09:59 +0300

    all the falsy values so far.

--- headFirstJavaScript/randomForNow/main.js
+++ headFirstJavaScript/randomForNow/main.js
@@ -1,37 +1,13 @@
-function findCarInLot(car) {
-    let i = 0;
-   while(i < lot.length){
-    if (car === lot[i]) {
-    return i;
-    }
-    i++;
-    }
-    return -1;
-   }
-   var chevy = {
-    make: "Chevy", 
-    model: "Bel Air"
-   };
-   var taxi = {
-    make: "Webville Motors", 
-    model: "Taxi"
-   };
-   var fiat1 = {
-    make: "Fiat", 
-    model: "500"
-   };
-   var fiat2 = {
-    make: "Fiat", 
-    model: "500"
-   };
-   var lot = [chevy, taxi, fiat1, fiat2];
-
-let loc1 = findCarInLot(fiat2);
-console.log(loc1);
-let loc2 = findCarInLot(taxi);
-console.log(loc2);
-let loc3 = findCarInLot(chevy);
-console.log(loc3);
-let loc4 = findCarInLot(fiat1);
-console.log(loc4);
-   
+let test; 
+if (test) {
+    console.log(`test is truthy`);
+}
+if (0) {
+    console.log(`0 is truthy`);
+}
+if("") {
+    console.log(`"" is truthy`);
+}
+if (NaN) {
+    console.log(`NaN is truthy`);
+}
