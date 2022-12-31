ca59d1f92f2e68e067439bde1a24767e328c2844
Mohammed-Aali <alalimohammed258@gmail.com>
2022-12-31 05:03:12 +0300

    the typeof functin first use

--- headFirstJavaScript/randomForNow/main.js
+++ headFirstJavaScript/randomForNow/main.js
@@ -1,19 +1,11 @@
-function addSongs() {
-    let song1 = document.getElementById("song1");
-    let song2 = document.getElementById("song2");
-    let song3 = document.getElementById("song3");
-
-    song1.innerHTML = "Blue Sued strings, by Elvis Pagely";
-    song1.setAttribute("class", "redtext");
-    song2.innerHTML = "Young God, by Russ";
-    let c = song1.getAttribute("ass");
-    song3.innerHTML = "Candy shop, By 50cents";
-    
-    if (c == null) {
-        console.log(`Hmm, strange there seems to be no attribute.`)
-    } else {
-        console.log(`found it ${c}`);
-    }
+function prop(type) {
+    console.log(typeof type);
 }
-
-window.onload = addSongs;
+prop("abcd");
+prop(123);
+prop({});
+prop([]);
+prop();
+prop({"abcd" : 123});
+prop(["abcd", 123]);
+prop(() => {return "abcd"});
