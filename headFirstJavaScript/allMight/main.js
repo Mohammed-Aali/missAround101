d18a040a9c6dabcb07d4ac4dc2b63d314300a898
Mohammed-Aali <alalimohammed258@gmail.com>
2023-01-02 15:56:24 +0300

    were almost close to finishing this damn game

--- headFirstJavaScript/allMight/main.js
+++ headFirstJavaScript/allMight/main.js
@@ -19,9 +19,9 @@ let model = {
     shipLength: 3,
     shipsSunk: 0,
 
-    ships: [ { locations: ["10", "20", "30"],
-    hits: ["", "", ""] }, {locations: ["32", "33", "34"],
-    hits: ["", "", ""] }, { locations: ["63", "64", "65"],
+    ships: [ { locations: ["06", "16", "26"],
+    hits: ["", "", ""] }, {locations: ["24", "34", "44"],
+    hits: ["", "", ""] }, { locations: ["10", "11", "12"],
     hits: ["", "", ""]} ],
 
     fire: function(guess){
@@ -40,7 +40,7 @@ let model = {
             }
         }
         view.displayMiss(guess);
-        displayMessage("You missed.");
+        view.displayMessage("You missed.");
         return false;
     },
 
@@ -52,7 +52,41 @@ let model = {
         }
         return true;
     }
-
- 
 };
 
+let controller = {
+    guesses: 0,
+
+    processGuess: function(guess) {
+        let location = parseGuess(guess);
+        if (location) {
+            this.guesses++;
+            let hit = model.fire(location);
+        }
+    }
+};
+function parseGuess(guess) {
+    let alphabet = ["A", "B", "C", "D", "E", "F", "G" ]
+    if (guess === null || guess.length !== 2) {
+        alert("Oops, please enter a letter and a number on the board.");
+    } else {
+        firstChar = guess.charAt(0);
+        let row = alphabet.indexOf(firstChar);
+        let column = guess.charAt(1);
+
+        if (isNaN(row) || isNaN(column)) {
+            alert(`Oops, this is not on the board`);
+        } else if (row < 0 || row >= model.boardSize || column >= model.boardSize || column < 0 ) {
+            alert(`Oops, that's off the board`);
+        } else {
+            return row + column;
+        }
+    }
+    return null;
+}
+
+console.log(parseGuess("A0"));
+console.log(parseGuess("B6"));
+console.log(parseGuess("G3"));
+console.log(parseGuess("H0"));
+console.log(parseGuess("A7"));
