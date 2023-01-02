eada50d9b91e9cd88088f09aed16e1c4d1983f3c
Mohammed-Aali <alalimohammed258@gmail.com>
2023-01-02 06:21:14 +0300

    the battle ship initial stages

--- headFirstJavaScript/allMight/main.js
+++ headFirstJavaScript/allMight/main.js
@@ -11,4 +11,14 @@ let view = {
         let cell = document.getElementById(shipLocation);
         cell.setAttribute("class", "miss");
     }
-}
+};
+
+let ships = [ { locations: ["10", "20", "30"],
+hits: ["", "", ""] }, {locations: ["32", "33", "34"],
+hits: ["", "", ""] }, { locations: ["63", "64", "65"],
+hits: ["", "", "hit"]} ];
+
+let shit2 = ships[1];
+let locations = shit2.locations;
+console.log(`The location of the second ship is ${locations[1]}`);
+
