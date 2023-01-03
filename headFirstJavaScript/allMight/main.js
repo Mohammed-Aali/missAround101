2cc7e0d5122b086da04ac284f23cd284e225fa6f
Mohammed-Aali <alalimohammed258@gmail.com>
2023-01-03 08:40:53 +0300

    the final version of the battleship game, although it was difficult and i understood what was happening with the code, i know this is byond my current level of skills to build a game of this scale, but I will try harder.

--- headFirstJavaScript/allMight/main.js
+++ headFirstJavaScript/allMight/main.js
@@ -19,9 +19,9 @@ let model = {
     shipLength: 3,
     shipsSunk: 0,
 
-    ships: [ { locations: ["06", "16", "26"],
-    hits: ["", "", ""] }, {locations: ["24", "34", "44"],
-    hits: ["", "", ""] }, { locations: ["10", "11", "12"],
+    ships: [ { locations: ["", "", ""],
+    hits: ["", "", ""] }, {locations: ["", "", ""],
+    hits: ["", "", ""] }, { locations: ["", "", ""],
     hits: ["", "", ""]} ],
 
     fire: function(guess){
@@ -61,6 +61,39 @@ let model = {
             } while (this.collision(locations));
             this.ships[i].locations = locations;
         }
+    },
+    generateShip: function() {
+        let direction = Math.floor(Math.random() * 2);
+        let row, col;
+
+        if(direction === 1) {
+            row = Math.floor(Math.random() * this.boardSize);
+            col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+        } else {
+            row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+            col = Math.floor(Math.random() * this.boardSize);
+        }
+
+        let newShipLocation = [];
+        for (let i = 0; i < this.shipLength; i++) {
+            if (direction === 1) {
+                newShipLocation.push(row + `` + (col + i));
+            } else {
+                newShipLocation.push((row + i) + `` + col);
+            }
+        }
+        return newShipLocation;
+    },
+    collision: function(locations) {
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = model.ships[i];
+            for(let j = 0; j < locations.length; j++){
+                if(ship.locations.indexOf(locations[j]) >= 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 };
 
@@ -103,6 +136,7 @@ function init() {
     fireButton.onclick = handleFireButton;
     let guessInput = document.getElementById("guessInput");
     guessInput.onkeypress = handleKeyPress;
+    model.generateShipLocations();
 }
 
 function handleKeyPress(e) {
@@ -120,7 +154,5 @@ function handleFireButton() {
 
     guessInput.value = "";
 }
-
 window.onload = init;
 
-console.log(Math.floor(Math.random() * 2))
