716cb376ba099cfe934cb34e69b648acb76d38cd
Mohammed-Aali <alalimohammed258@gmail.com>
2023-01-04 09:21:30 +0300

    fully functional img unblurr machine

--- headFirstJavaScript/allMight/main.js
+++ headFirstJavaScript/allMight/main.js
@@ -1,5 +1,16 @@
-function pageLoadHandler() {
-    alert(`I'm Alive!`)
-};
+window.onload = init;
+function init() {
+   let img = document.getElementsByTagName("img");
+   for(let i = 0; i < img.length; i++) {
+    img[i].onclick = showAnswer;
+   }
+}
 
-window.onload = pageLoadHandler;
+function showAnswer(eventObj){
+    let img = eventObj.target;
+    let pathWay = `/headFirstJavaScript/Head-First-JavaScript-Programming-master/Head-First-JavaScript-Programming-master/chapter9/`
+    let name = img.id;
+    name = `${pathWay}${name}.jpg`
+    img.src = name;
+}
+
