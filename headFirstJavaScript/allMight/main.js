040ea7fc0e5e839cd2205b57608db3f28c119204
Mohammed-Aali <alalimohammed258@gmail.com>
2023-01-06 11:41:33 +0300

    the first localstorage usage with js

--- headFirstJavaScript/allMight/main.js
+++ headFirstJavaScript/allMight/main.js
@@ -1,8 +1,11 @@
 const input = document.querySelector(`input`);
 const h1 = document.querySelector(`h1`);
 
+h1.innerHTML = localStorage.getItem(`hello`);
 input.addEventListener(`keyup`, display);
 
 function display() {
-    h1.innerHTML = input.value;
+    localStorage.setItem(`hello`, input.value);
+    h1.innerHTML = localStorage.getItem(`hello`);
 }
+
