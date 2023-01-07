6f5af3ea11c711ee47fd971e8750ba8893fadf40
Mohammed-Aali <alalimohammed258@gmail.com>
2023-01-07 10:07:36 +0300

    done with headFirst js

--- headFirstJavaScript/allMight/main.js
+++ headFirstJavaScript/allMight/main.js
@@ -1,48 +1,10 @@
-let passengers = [ 
-    {name: `Jane Deloop`, paid: true},
-    {name: `Dr. Eval`, paid: false},
-    {name: `Sue property`, paid: false},
-    {name: `John funcall`, paid: false}
- ] 
+function sayIt(translator) {
+    let phrase = translator("Hello");
+    alert(phrase);
+}
+function hawaiianTranslator(word) {
+    if (word === "Hello") {return `Aloha`};
+    if (word === "Goodbye") {return `Aloha`};
+}
 
- function checkPaid(array) {
-    let hasNotPaid = [];
-    for (let i = 0; i < array.length; ++i) {
-        if (!array[i].paid) {
-           hasNotPaid.push(array[i].name);
-        }
-    }
-    return hasNotPaid;
- }
- console.log(checkPaid(passengers))
-
- function checkFlyList(array1, array2) {
-    for(let i = 0; i < array1.length; i++) {
-        for (let j = 0; j < array2; j++) {
-            if (array1[i].name === array2[j]) {
-                return array2[j] + `this passanger is on the no fly list`
-            }
-        }
-        return `plane is good for takeoff`
-    }
- }
-
- function checkPassengers(array) {
-    let allOnFlight = [];
-    for (let i = 0; i < array.length; i++) {
-        if (array[i].paid) {
-            
-            allOnFlight.push(array[i].name);
-            return allOnFlight;
-        } else {
-            alert(`plane is not clear for takeOff`)
-        }
-    }
- } 
-
-
-
-
-
-
-
+sayIt(hawaiianTranslator);
