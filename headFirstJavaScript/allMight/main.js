52024f109dd07e24469a06b5e7af44b8ea1dea09
Mohammed-Aali <alalimohammed258@gmail.com>
2023-01-09 07:50:09 +0300

    excercise with an anonymous functions

--- headFirstJavaScript/allMight/main.js
+++ headFirstJavaScript/allMight/main.js
@@ -1,55 +1,22 @@
-let products = [  { name: "Grapefruit", calories: 170, color: "red", sold: 8200 },
-{ name: "Orange", calories: 160, color: "orange", sold: 12101 },
-{ name: "Cola", calories: 210, color: "caramel", sold: 25412 },
-{ name: "Diet Cola", calories: 0, color: "caramel", sold: 43922 },
-{ name: "Lemon", calories: 200, color: "clear", sold: 14983 },
-{ name: "Raspberry", calories: 180, color: "pink", sold: 9427 },
-{ name: "Root Beer", calories: 200, color: "caramel", sold: 9909 },
-{ name: "Water", calories: 0, color: "clear", sold: 62123 } ];
-
-function compareSold(colaA, colaB) {
-    return colaA.sold - colaB.sold;
+window.onload = function() {
+    var button = document.getElementById("bake");
+    button.onclick = function(){console.log("Time to bake the cookies.");
+    cookies.bake(2500);
 }
+};
+var cookies = {
+ instructions: "Preheat oven to 350...",
+ bake: function(time) {
+ console.log("Baking the cookies.");
+ setTimeout(done, time);
+ }
+};
 
-function printProducts(products) {
-    for(let i = 0; i < products.length; i++) {
-        console.log(`Name: ${products[i].name}, Calories: ${products[i].calories}, Color: ${products[i].color}, Sold: ${products[i].sold}`)
-    }
+function done() {
+ alert("Cookies are ready, take them out to cool.");
+ console.log("Cooling the cookies.");
+
+ setTimeout(function() {
+    alert("Cookies are cool, time to eat!")
+ }, 1000);
 }
-
-function compareName(colaA, colaB){
-    if(colaA.name > colaB.name) {
-        return 1;
-    } else if (colaA.name === colaB.name) {
-        return 0;
-    } else {
-        return -1;
-    }   
-}
-
-function compareCalories(colaA, colaB){
-    return colaA.calories - colaB.calories;
-}
-
-function compareColor(colaA, colaB) {
-    if(colaA.color > colaB.color) {
-        return 1;
-    } else if (colaA.color === colaB.color) {
-        return 0;
-    } else {
-        return -1;
-    }   
-}
-
-products.sort(compareName);
-console.log(`products sorted by name:`);
-printProducts(products);
-
-products.sort(compareCalories);
-console.log(`products sorted by Calories:`);
-printProducts(products);
-
-products.sort(compareColor);
-console.log(`products sorted by ColorcompareColor:`);
-printProducts(products);
-
