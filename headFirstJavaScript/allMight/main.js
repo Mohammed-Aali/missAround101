c2caf0da780ae69ffb2ccf25fe62a7de24af7d0d
Mohammed-Aali <alalimohammed258@gmail.com>
2023-01-10 07:38:30 +0300

    the introduction of the closure function concept

--- headFirstJavaScript/allMight/main.js
+++ headFirstJavaScript/allMight/main.js
@@ -1,12 +1,43 @@
-let secret = `007`;
-
-function getSecret() {
-    let secret = `008`;
-
-    function getValue(){
-        return secret;
+function makePassWord(password){
+    return function passWordGuess(passwordGuess) {
+        return (passwordGuess === password);
     }
-    return getValue
 }
-let getValueFun = getSecret();
-console.log(getValueFun())
+let tryGuess = makePassWord(`secret`);
+console.log(`Guessing 'nope': ${tryGuess(`nope`)}`)
+console.log(`Guessing 'secret': ${tryGuess(`secret`)}`)
+
+
+
+function multN(n) {
+    return function multiB(b){
+        return n * b;
+    }
+}
+
+let multi = multN(6);
+console.log(`when you multiply 6 by 7 you get ${multi(7)}`)
+console.log(`when you multiply 6 by 10 you get ${multi(10)}`)
+
+function makeCounter() {
+    let count = 0;
+    return {
+        method: function increment(){
+            count++
+            return count;
+        }
+    }
+}
+
+let counter = makeCounter();
+console.log(counter.method());
+console.log(counter.method());
+console.log(counter.method());
+
+makeTimer("Cookies are done!", 1000);
+function handler() {
+    alert(doneMessage);
+}
+function makeTimer(doneMessage, n) {
+setTimeout(handler, n);
+}
