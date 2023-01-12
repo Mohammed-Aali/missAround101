cf462fa9c328b4e13226ed6b81f562e2c6cfe45d
Mohammed-Aali <alalimohammed258@gmail.com>
2023-01-12 11:43:23 +0300

    its finally done boiz, i thought i would never finish it lol, good thing its done time to move on to bigger thing, and as a reward i get a 20 minute break, only for this once tho.

--- toDoList/toDoList3/script.js
+++ toDoList/toDoList3/script.js
@@ -7,8 +7,10 @@ const listTitleElement = document.querySelector("[data-list-title]")
 const listCountElement = document.querySelector("[data-list-count]")
 const tasksContainer = document.querySelector('[data-tasks]')
 const taskTemplate = document.getElementById('task-template')
+const newTaskForm = document.querySelector("[data-new-task-form]")
+const newTaskInput = document.querySelector("[data-new-task-input]")
+const clearCompleteTaskButton = document.querySelector("[data-clear-complete-tasks-button]")
 
-console.log(taskTemplate);
 
 const LOCAL_STORAGE_LIST_KEY = "task.lists"
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId"
@@ -20,6 +22,22 @@ listContainer.addEventListener('click', e => {
         selectedListId = e.target.dataset.listId
         saveAndRender();
     }
+})
+
+tasksContainer.addEventListener("click", e => {
+    if(e.target.tagName.toLowerCase() === "input") {
+        const selectedList = lists.find(list => list.id === selectedListId)
+        const selectedTask = selectedList.tasks.find(task => task.id === e.target.id)
+        selectedTask.complete = e.target.checked
+        save()
+        renderTaskCount(selectedList)
+    }
+})
+
+clearCompleteTaskButton.addEventListener("click", e => {
+    const selectedList = lists.find(list => list.id === selectedListId)
+    selectedList.tasks = selectedList.tasks.filter(task => !task.complete)
+    saveAndRender()
 })
 
 deleteListButton.addEventListener("click", e => {
@@ -38,8 +56,23 @@ newListForm.addEventListener("submit", e => {
     saveAndRender();
 })
 
+newTaskForm.addEventListener("submit", e => {
+    e.preventDefault()
+    const taskName = newTaskInput.value
+    if(taskName == null || taskName === "") return
+    const task = createTask(taskName)
+    newTaskInput.value = null
+    const selectedList = lists.find(list => list.id === selectedListId)
+    selectedList.tasks.push(task)
+    saveAndRender();
+})
+
 function createList(name) {
     return { id: Date.now().toString(), name: name, tasks: [] }
+}
+
+function createTask(name) {
+    return { id: Date.now().toString(), name: name, complete: false }
 }
 
 function saveAndRender(){
@@ -65,14 +98,21 @@ function render() {
         listTitleElement.innerText = selectedList.name
         renderTaskCount(selectedList)
         clearElement(tasksContainer)
-        renderTaskCount(selectedList)
+        renderTasks(selectedList)
     }
 }
 
 function renderTasks(selectedList) {
-    selectedList.tasks.forEach(task => {
-        const tasksElement = document.importNode(taskTemplate.contentEditable, true)
-    })
+     selectedList.tasks.forEach(task => {
+    const taskElement = document.importNode(taskTemplate.content, true)
+    const checkbox = taskElement.querySelector('input')
+    checkbox.id = task.id
+    checkbox.checked = task.complete
+    const label = taskElement.querySelector('label')
+    label.htmlFor = task.id
+    label.append(task.name)
+    tasksContainer.appendChild(taskElement)
+  })
 }
 
 function renderTaskCount(selectedList) {
