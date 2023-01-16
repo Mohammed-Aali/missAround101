89c73def0ac09f673e0023345da14ddfbc1c0396
Mohammed-Aali <alalimohammed258@gmail.com>
2023-01-16 15:38:17 +0300

    the spread operator date

--- random/easy.js
+++ random/easy.js
@@ -6,3 +6,11 @@ const initialValue = 10;
 let sumWithInitial = banana.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
 
 console.log(sumWithInitial)
+
+const sum = (...args) => args.reduce((a, b) => a + b , 0);
+
+const baseArray = [1, 2, 3, 4, 5]
+console.log(sum(2,3,4,5,6,6,7,8,5,4,3,3))
+
+let x = Math.max(...baseArray)
+console.log(x);
