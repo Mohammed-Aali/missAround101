671d0328c54b52e014532611f10f1da368be810e
Mohammed-Aali <alalimohammed258@gmail.com>
2023-01-19 16:49:21 +0300

    fix an issue

--- headFirstHTML/script.js
+++ headFirstHTML/script.js
@@ -1,25 +1,25 @@
-let drink = "RedBull";
-let lyrics = "";
-let cans = 99;
+// let drink = "RedBull";
+// let lyrics = "";
+// let cans = 99;
 
-while (cans > 0) {
-    lyrics = `${lyrics} ${cans} cans of ${drink} on the wall <br>`
-    lyrics = `${lyrics} ${cans} cans of ${drink} <br>`
-    lyrics = `${lyrics} Take one down, pass it around, <br>`
-    if (cans > 1) {
-        lyrics+= `${cans-1} cans of ${drink} on the wall <br>`
-    } else {
-        lyrics+= `no more cans of ${drink} on the wall <br>`
-    }
-    cans--;
-}
-document.write(lyrics);
+// while (cans > 0) {
+//     lyrics = `${lyrics} ${cans} cans of ${drink} on the wall <br>`
+//     lyrics = `${lyrics} ${cans} cans of ${drink} <br>`
+//     lyrics = `${lyrics} Take one down, pass it around, <br>`
+//     if (cans > 1) {
+//         lyrics+= `${cans-1} cans of ${drink} on the wall <br>`
+//     } else {
+//         lyrics+= `no more cans of ${drink} on the wall <br>`
+//     }
+//     cans--;
+// }
+// document.write(lyrics);
 
 bitches = ['maria', 'Ghadeer', "Shahed", 'hannah', 'fatima', 'zainab', 'maha', 'amjad', 'ahed'];
 console.log(bitches);
-let i = bitches.reverse().length - 1;
-while (i > 0) {
+let i = 0;
+while (i < bitches.length) {
     console.log(`I love you ${bitches[i]} and I'll marry you in the future `)
-    i--;
+    i++;
 }
 
