7dfee1fbd59d00a70432d719e25eb937c73055f9
Mohammed-Aali <alalimohammed258@gmail.com>
2023-01-23 05:04:09 +0300

    the start of today's session

--- headFirstHTML/script.js
+++ headFirstHTML/script.js
@@ -35,17 +35,22 @@
 //     console.log(berries);
 // }
 
-function makePhrases() 
-        {
-        let words1 = ["24/7", "multi-Tier", "30,000 feet", "B-to-B", "win-win"];
-        let words2 = ["empowered", "value-added", "oriented", "focused", "aligned"];
-        let words3 = ["process", "solution", "tipping-point", "strategy", "vision"];
-        let rand1 = Math.floor(Math.random() * words1.length);
-        let rand2 = Math.floor(Math.random() * words2.length);
-        let rand3 = Math.floor(Math.random() * words3.length);
-        let phrase = words1[rand1] + " " + words2[rand2] + " " + words3[rand3];
-        let phraseElement = document.getElementById("phrase");
-        phraseElement.innerHTML = phrase
-        }
+
+function makePhrases() {
+        let subject = ["I", "He", "She", "they", "them is"];
+        let verb = ["banged", "ate", "vomited", "ran", "tasted"];
+        let noun = ["Lisa", "Ice", "WW3", "Noodles"];
+        const subjectL = subject.length;
+        let rand1 = Math.floor(Math.random() * subjectL);
+        const verbL = verb.length;
+        let rand2 = Math.floor(Math.random() * verbL);
+        const nounL = noun.length;
+        let rand3 = Math.floor(Math.random() * nounL);
+
+        let phrase = `${subject[rand1]} ${verb[rand2]} ${noun[rand3]}`
+        const phraseElement = document.getElementById("phrase");
+        phraseElement.innerHTML = phrase;
+}
         window.onload = makePhrases;
+        console.log([])
     
