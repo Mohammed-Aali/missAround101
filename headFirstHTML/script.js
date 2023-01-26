e62adef87850669b7195d1694eda248aafb55eff
Mohammed-Aali <alalimohammed258@gmail.com>
2023-01-26 16:55:35 +0300

    the end of todays session

--- headFirstHTML/script.js
+++ headFirstHTML/script.js
@@ -1,27 +1,6 @@
-let guessInput = document.getElementById("guessInput");
-let guess = guessInput.value;
-console.log(guess)
-
-
-checkAnswer(guess);
-
-function checkAnswer(guess) {
-        let answers = ["red", "blue", "green"];
-        let index = Math.floor(Math.random() * answers.length)
-
-        if (guess == answers[index]) {
-                replyWith = `Holy shit! You Are correct, I was thinking of ${answers[index]}`
-        } else {
-                replyWith = `Sorry, you dummy. I was thinking of ${answers[index]}`
-        }
-        return replyWith;
+function addOne(one) {
+        return one + 1;
 }
+let plusOne = addOne;
 
-console.log(checkAnswer())
-
-
-
-
-
-
-
+console.log(plusOne(68))
