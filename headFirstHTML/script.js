983347fefbfc38ef8547514257d72eb0669f4005
Mohammed-Aali <alalimohammed258@gmail.com>
2023-01-26 05:05:39 +0300

    the start of today's session

--- headFirstHTML/script.js
+++ headFirstHTML/script.js
@@ -1,23 +1,27 @@
-
-function init() {
-        const addButton = document.getElementById("addButton");
-        addButton.addEventListener("click", handleButtonClick)
-        loadPlaylist();
-}
-
-window.onload = init;
+let guessInput = document.getElementById("guessInput");
+let guess = guessInput.value;
+console.log(guess)
 
 
-function handleButtonClick() {
-        const songTextInput = document.getElementById("songTextInput");
-        const li = document.createElement("li");
-        const ul = document.getElementById("playlist")
-        songName = songTextInput.value;
-        if (songName === "") {
-        return alert("Please enter a value first");
+checkAnswer(guess);
+
+function checkAnswer(guess) {
+        let answers = ["red", "blue", "green"];
+        let index = Math.floor(Math.random() * answers.length)
+
+        if (guess == answers[index]) {
+                replyWith = `Holy shit! You Are correct, I was thinking of ${answers[index]}`
+        } else {
+                replyWith = `Sorry, you dummy. I was thinking of ${answers[index]}`
         }
-        li.innerHTML = songName;
-        ul.appendChild(li);
-        save(songName);
+        return replyWith;
 }
+
+console.log(checkAnswer())
+
+
+
+
+
+
 
