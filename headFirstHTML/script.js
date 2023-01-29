029e1ac44b555e36884892a05334f3b2fb574112
Mohammed-Aali <alalimohammed258@gmail.com>
2023-01-29 10:14:37 +0300

    the start of css and html

--- headFirstHTML/script.js
+++ headFirstHTML/script.js
@@ -1,38 +1,27 @@
 let movie1 = {
-        title: "Plan 9 from outer space",
-        showTimes: ["3:00pm", "7:00pm", "11:00pm"], 
-        genre: "cult classic",
-        rating: 2
-}
-
-let movie2 = {
-        title: "Forbidden Planet",
-        showTimes: ["5:00pm", "9:00pm"], 
-        genre: "classic sci-fi",
-        rating: 5
-}
-
-function getNextShowing(movie) {
-        let now = new Date().getTime();
-
-        for (let i = 0; i < movie.showTimes.length; i++){
-                let showTime = getTimeFromString(movie.showTimes[i]);
-                if ((showTime - now) > 0) {
-                        return `Next Showing of ${movie.title} is ${movie.showTimes[i]}`
-                }
+        title: "Plan 9 from Outer Space",
+        genre: "Cult Classic",
+        rating: 5,
+        showtimes: ["3:00pm", "7:00pm", "11:00pm"],
+        getNextShowing: function() {
+            let now = new Date().getTime();
+        for (let i = 0; i < this.showtimes.length; i++){
+            let showtime = getTimeFromString(this.showtimes[i]);
+        if ((showtime - now) > 0) {
+            return "Next showing of " + this.title + " is " + this.showtimes[i];
+            }
         }
         return null;
+        }
 }
 
+
 function getTimeFromString(timeString) {
-        let theTime = new Date();
+        var theTime = new Date();
         var time = timeString.match(/(\d+)(?::(\d\d))?\s*(p?)/);
         theTime.setHours( parseInt(time[1]) + (time[3] ? 12 : 0) );
         theTime.setMinutes( parseInt(time[2]) || 0 );
-        return theTime.getTime();
+        return theTime.getTime()
 }
 
-let nextShowing = getNextShowing(movie1);
-console.log(nextShowing);
-nextShowing = getNextShowing(movie2);
-console.log(nextShowing);
+//////// this new line is for dogs only and we're talking constructor
