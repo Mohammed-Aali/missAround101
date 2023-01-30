f9ef0a4884d536163ce6e1fe2e527c6b89a7d785
Mohammed-Aali <alalimohammed258@gmail.com>
2023-01-30 05:05:21 +0300

    the start of today's sesion

--- headFirstHTML/script.js
+++ headFirstHTML/script.js
@@ -25,3 +25,43 @@ function getTimeFromString(timeString) {
 }
 
 //////// this new line is for dogs only and we're talking constructor
+
+function Dog(name, breed, weight) {
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+    this.bark = function () {
+        if(this.weight > 25) {
+            console.log(`${this.name} says woof!`);
+        } else {
+            console.log(`${this.name} says Yip!`)
+        }
+    }
+}
+
+let fido = new Dog("Fido", "Mixed", 67) 
+let tiny = new Dog("Tiny", "Chawalla", 8)
+let clifford = new Dog("Clifford", "Bloodhound", 65)
+
+function Movie(title, genre, rating, showtimes) {
+    this.title = title;
+    this.genre = genre;
+    this.rating = rating;
+    this.showtimes = showtimes;
+    this.getNextShowing = function() {
+        let now = new Date().getTime()
+        for(let i = 0, n = this.showtimes.length; i < n; i++) {
+            let showtime = getTimeFromString(this.showtimes[i])
+            if ((showtime - now) > 0) {
+                return `Next showing of ${this.title} is ${this.showtimes[i]}`
+            }
+           
+        }
+    }
+}
+
+let banzaiMovie = new Movie("Buckaroo Banzai", "Cult Classic", 5, ["1:00pm", "5:00pm", "7:00pm", "11:00pm"])
+
+let plan9Movie = new Movie("Plan 9 from Outer Space", "Cult Classic", 2, ["3:00pm", "7:00pm", "11:00pm"]);
+
+console.log(plan9Movie.getNextShowing())
