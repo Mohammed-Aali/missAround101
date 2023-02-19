f4cfff5786d97fb8124278ed302105f378b82320
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-19 05:18:38 +0000

    Feb 19, 2023, 8:18 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -2,6 +2,8 @@
   let name = prompt()
   if(name === "Mohammed") { // identity operator // strict equality
     console.log("Welcome")
+  } else if (name === "sally") {
+    console.log("Welcome")
   } else {
     console.log("you are not wlecome")
   }
