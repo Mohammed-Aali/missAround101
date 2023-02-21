c723a00ba62491def5cc7f0916053ca7d1e66239
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-21 07:03:23 +0000

    Feb 21, 2023, 10:03 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -1,23 +1,9 @@
-pow.calculated = {};
 function pow(x, y) { 
-  let stringVersion = x + "^" + y;
-  console.log(`string version ` + stringVersion)
-  if(stringVersion in pow.calculated) {
-    console.log("Found");
-    return pow.calculated[stringVersion];
-  }
-  console.log("CALCULATING: ")
   let total = 1; 
   for(let i = 0; i < y; i++) {
     total *= x;
   }
-  //pow.calculated.push(total)
-  pow.calculated[stringVersion] = total;
-  console.log(pow.calculated)
-  return total;
 }
 
-pow(3, 3);
-pow(3, 3);
-pow(10, 3);
-pow(6, 2);
+console.log(pow())
+console.log(pow(1,2,3,4,5))
