2c0a7452a4e9a5c4b53cb6d9375b463cfc8810d3
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-21 06:36:54 +0000

    Feb 21, 2023, 9:36 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -1,5 +1,22 @@
-{
-  let x = new Date(2012, 11, 0, 6)
-  console.log(x.getTimezoneOffset() / 60);
-  console.log(x)
+function pow(x, y) {
+  let total = 1; 
+  for(let i = 0; i < y; i++) {
+    total *= x;
+  }
+  return total;
 }
+// let coolFuntions = [pow]
+// console.log(coolFuntions[0](2,3))
+
+let mathFunction = {
+  power: pow
+}
+
+pow.description = "i love banana"
+console.log(pow.description)
+
+function returnFunction(func) {
+  return func;
+}
+
+console.log(callBack(pow))
