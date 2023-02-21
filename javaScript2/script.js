ed90f85dcc480c62cc91511e8f79282f857ce909
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-21 06:42:55 +0000

    Feb 21, 2023, 9:42 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -1,7 +1,12 @@
+pow.calculated = []
 function pow(x, y) {
   let total = 1; 
   for(let i = 0; i < y; i++) {
     total *= x;
   }
+  pow.calculated.push(total)
+  console.log(pow.calculated)
   return total;
 }
+
+pow(8, 9);
