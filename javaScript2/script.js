278e9fdc506f9773683ce70acac17d76c07d871d
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-22 03:20:53 +0000

    Feb 22, 2023, 6:20 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -1,20 +1,3 @@
-{
-  // the value of this to the arrow function depends on where it was originally created 
-  let arrow = () => this;
-  function normal() {
-    return this;
-  }
-  console.log(normal())
-  console.log(arrow())
-
-  let functions = {
-    this: this,
-    arrow: arrow,
-    normal: normal,
-    arrowTest: () => this
-  }
-  console.log(functions.arrow())
-  console.log(functions.normal())
-  // returns the window object
-  console.log(functions.arrowTest())
-}
+console.log(this);
+let arrow = () => this;
+console.log(arrow());
