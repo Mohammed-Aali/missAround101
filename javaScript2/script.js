10f0f14b261c05cc2d602d694f76199d06d5c25d
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-22 06:22:35 +0000

    Feb 22, 2023, 9:22 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -1,21 +1,11 @@
 {
+  // normal constructor 
+  // this is the recommended way
   function User(name, intrest){
     this.name = name;
     this.intrest = intrest;
   }
-
-  function newUser(name, intrest) {
-    let person = {
-      name: name,
-      intrest: intrest
-    };
-
-    return person;
-  }
-
   let me = new User("Mohammed", ['talking to people', "dancing", "Eating"]);
   let you = newUser("Hermis", ["Hotwheels"])
-  console.log(me);
-  console.log(you);
-  me.membership = "gold";
+  console.log(me, you);
 }
