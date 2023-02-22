0cdd8507152d09d40530d05c6ac568e7fec237cd
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-22 05:19:38 +0000

    Feb 22, 2023, 8:19 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -2,10 +2,4 @@ function doSomething() {
   throw {error: "Its broke", code: -1}
 }
 
-try {
-  doSomething();
-} catch(error) {
-  console.log(error);
-} finally {
-  console.log("wrapping things up... ");
-} 
+doSomething();
