3c415233b0860561c94b9fcf7842bf20510aa11b
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-22 02:38:29 +0000

    Feb 22, 2023, 5:38 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -2,6 +2,6 @@ function doStuff(input, input2) {
   console.log(input + "\n" + input2)
   console.log(this)
 }
-
-let x = doStuff.bind("Hello", 8, 9);
+let me = {name: "Mohammed"}
+let x = doStuff.bind(me, 8, 9);
 x();
