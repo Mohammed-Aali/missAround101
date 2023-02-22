67ae075aee11c81788f4b7977d2faed8bd019ae0
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-22 02:48:06 +0000

    Feb 22, 2023, 5:48 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -1,6 +1,9 @@
 function cube(x) {
   return x*x*x;
 }
-let cubeArrow = x => x*x*x
+let cubeArrow = x => {
+  console.log("Calculating...");
+  return x*x*x 
+};
 console.log(cube(6));
 console.log(cubeArrow(6));
