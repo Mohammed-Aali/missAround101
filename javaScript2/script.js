e34cd3911922bb7b8352edc639a3d1e292de9d71
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-22 07:34:09 +0000

    Feb 22, 2023, 10:34 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -3,10 +3,17 @@
     active: true,
   };
 
+  let student = {
+    major: "English"
+  }
   let teacher = {
     teaching: ["Math", "Science"]
   }
 
   Object.setPrototypeOf(teacher, user);
+  Object.setPrototypeOf(student, user);
+
+
   console.log(teacher);
+  console.log(student);
 }
