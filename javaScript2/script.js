956b3a73fb38f53d2659e2144eee9e9f539408d6
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-22 02:14:53 +0000

    Feb 22, 2023, 5:14 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -5,6 +5,11 @@ let me = {
 function outputMe() {
   console.log(this);
 }
+function outputMeStrict(){
+    'use strict';
+    console.log(this);
+}
 
-outputMe()
-me.outputMe()
+outputMe() // function
+me.outputMe() // method
+outputMeStrict() // function with strict mode
