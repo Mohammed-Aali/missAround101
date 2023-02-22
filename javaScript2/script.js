9f9b47def2c5cf8a58be43452e57cbb44b6f2072
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-22 06:14:23 +0000

    Feb 22, 2023, 9:14 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -1,8 +1,9 @@
 {
-  function User(name){
+  function User(name, intrest){
     this.name = name;
+    this.intrest = intrest;
   }
-  let me = new User("Mohammed");
+  let me = new User("Mohammed", ['talking to people', "dancing", "Eating"]);
   let you = new User("Hermis")
   console.log(me);
   console.log(you);
