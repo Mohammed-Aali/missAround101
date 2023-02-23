f240f4c2ad1349aa4110580ece3ad6e6bc5c3a9d
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-23 08:41:15 +0000

    Feb 23, 2023, 11:41 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -1,22 +1,3 @@
 {
-  let list = document.getElementsByTagName("ol");
-  console.log(list);
-  let target = list[0];
-  console.log(target.children)
 
-  target.onmousemove = (e) => {
-    console.log("Mouse over");
-    target.children[0].innerHTML = "Hoes";
-  };
-
-  let button = document.getElementById("click");
-  button.onmouseenter = (e) => {
-
-    button.innerHTML = "click me please!!";
-  }
-  button.onmouseleave = (e) => {
-    button.innerHTML = "Don't Click me!"
-
-    target.remove();
-  }
 }
