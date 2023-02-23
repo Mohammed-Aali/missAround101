c4a42dee849e29a2bdf4125ec330466da8281874
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-23 08:16:23 +0000

    Feb 23, 2023, 11:16 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -1,10 +1,4 @@
 {
-  let paragraphs = document.getElementsByTagName("p");
-  console.log(paragraphs)
-
-  let strong = paragraphs[0].childNodes[0].childNodes[0]
-  strong.nodeValue = "Hollar";
-
   let list = document.getElementsByTagName("ol");
   console.log(list);
   let target = list[0];
