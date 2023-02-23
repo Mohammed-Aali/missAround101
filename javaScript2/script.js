58f9d539e14a355de9f5de84cc89009233313d0a
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-23 05:51:15 +0000

    Feb 23, 2023, 8:51 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -12,12 +12,14 @@ function Student(name, major) {
   this.name = name;
   this.major = major;
 }
+// setting the prototype to user
 Student.prototype = new User;
 
 function Teacher(name, teaching) {
   this.name = name;
   this.teaching = teaching; 
 }
+// setting the prototype to user
 Teacher.prototype = new User;
 Teacher.prototype.sayHello = function(){
   return "Teacher says hi";
@@ -29,4 +31,6 @@ let teacher1 = new Teacher("Caleb", ["Math", "Science"])
 // console.log(student1, teacher1);
 console.log(teacher1 instanceof Teacher);
 console.log(student1 instanceof User)
+
+teacher1 instanceof Student ? console.log("yup") : console.log("nope");
 }
