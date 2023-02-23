e15d11bd61fb06c4f5229db7abb01191b0ff3d68
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-23 05:38:28 +0000

    Feb 23, 2023, 8:38 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -26,45 +26,6 @@ console.log(student1);
 console.log(teacher1);
   // if the property or method is common to all instance of object
   // if you want a different property you can write it on the object
-  let user = {
-    active: false,
-    sayHello: function() {
-      return "Hello " + this.name
-    } 
-  };
-
-  let student = {
-    name: "god student",
-    major: "English"
-  }
-  let teacher = {
-    name: "Peasant Caleb",
-    teaching: ["Math", "Science"],
-    sayHello: function() {
-      let message = this.name + " Teaches "
-      this.teaching.forEach(subject => {
-        message+= subject + " ";
-      })
-      return message;
-    } 
-  }
-
-  Object.setPrototypeOf(teacher, user);
-  Object.setPrototypeOf(student, user);
-
-  // its setting false in the student object itslef not the prototype
-  student.active = true;
-  
-  let newMember = [teacher, student]
-
-  let properties = []
-
-  for(let prop in teacher) {
-    // this exculdes active from the list
-    if(teacher.hasOwnProperty(prop)) {
-      properties.push(prop)
-    }
-  }
 
 
   // it checks only in the object itself it doesn't go up the inheratince
