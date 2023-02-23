cbc7cd06bf6b58e56a064d72fd0091f99b424b67
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-23 07:36:46 +0000

    Feb 23, 2023, 10:36 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -1,4 +1,4 @@
-{
-  let list = document.childNodes[1].childNodes[2].childNodes[3].childNodes[1]
+
+  let list = document.childNodes[1].childNodes[2].childNodes[3].childNodes[1];
   console.log(list)
-}
+  console.log(list.parentElement)
