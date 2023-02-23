60c113431f8a9c60829de87da2a3969e5a98893e
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-23 05:39:11 +0000

    Feb 23, 2023, 8:39 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -22,16 +22,5 @@ function Teacher(name, teaching) {
 let student1 = new Student("beach ball", "English");
 let teacher1 = new Teacher("Caleb", ["Math", "Science"])
 
-console.log(student1);
-console.log(teacher1);
-  // if the property or method is common to all instance of object
-  // if you want a different property you can write it on the object
-
-
-  // it checks only in the object itself it doesn't go up the inheratince
-  // console.log("in teacher ", "active" in teacher);
-
-  // console.log("Name in teacher? ",  teacher.active !== undefined);
-
-  // console.log(teacher);
+console.log(student1, teacher1);
 }
