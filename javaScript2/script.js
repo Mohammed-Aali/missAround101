be2c71f8f960ca5db4799014b05ca402993ec1c9
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-23 08:23:33 +0000

    Feb 23, 2023, 11:23 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -2,10 +2,10 @@
   let list = document.getElementsByTagName("ol");
   console.log(list);
   let target = list[0];
-  console.log(target)
+  console.log(target.children)
 
   target.onmousemove = (e) => {
-    console.log("Mouse over")
-    target.childNodes[1].childNodes[0].nodeValue = "Hoes";
+    console.log("Mouse over");
+    target.children[0].innerHTML = "Hoes";
   };
 }
