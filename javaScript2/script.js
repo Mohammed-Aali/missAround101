7d86cb45fbc66bcabb72f35389f497e9bc69209a
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-23 07:53:49 +0000

    Feb 23, 2023, 10:53 AM

--- javaScript2/script.js
+++ javaScript2/script.js
@@ -1,11 +1,3 @@
 {
   let list = document.getElementsByTagName("li");
-  console.log(list[0].nodeName);
-
-
-  if(list[0].childNodes[0].nodeType === 1) {
-    console.log("Element");
-  } else if(list[0].childNodes[0].nodeType === 3) {
-    console.log("Text")
-  }
 }
