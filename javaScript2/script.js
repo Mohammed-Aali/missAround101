736ca51ade91ab1cd718544e7502d41423b3f147
Mohammed-Aali <alalimohammed258@gmail.com>
2023-02-24 05:59:02 +0000

    Feb 24, 2023, 8:59 AM

--- javaScript2/script.js
+++
@@ -1,8 +0,0 @@
-
-  let btn = document.getElementById("click");
-
-  btn.onclick = (e) => {
-    let node = document.createElement("li");
-    node.appendChild(document.createTextNode("new list"))
-    console.log(node);
-  }
